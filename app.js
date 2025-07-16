4f3c559fbafb562275551a9a24c66554103da2dd
LiorKirshner <liorkk7@gmail.com>
2025-07-16 13:51:11 +0300

    little fixes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const { handleCommand } = require('./commands/commandHandler');
-
+const { handleCommand } = require("./commands/commandHandler");
 
 function main() {
   const args = process.argv.slice(2);
   if (args.length === 0) {
-    console.log('✗ Error: No command provided');
-    console.log('Usage: node contacts.js [add|list|search|delete|help] [arguments]');
+    console.log("✗ Error: No command provided");
+    console.log("Usage: node app.js [add|list|search|delete|help] [arguments]");
     process.exit(1);
   }
   handleCommand(args);
